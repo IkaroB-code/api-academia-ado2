799e901ad4c8ff1de583c1ca1ed97a169b593c74
IkaroB-code <ikarooliveirabispo@gmail.com>
2026-06-07 06:14:09 -0300

    fix prisma client initialization with DATABASE_URL

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -3,8 +3,13 @@ import pkg from "@prisma/client";
 
 const { PrismaClient } = pkg;
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 
 export default prisma;
 
-// ...
