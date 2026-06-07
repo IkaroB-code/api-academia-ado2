1392422561e8c31f02acaa78a84ad56c286c969e
IkaroB-code <ikarooliveirabispo@gmail.com>
2026-06-07 04:39:32 -0300

    corrige prisma para PostgreSQL

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,19 +1,8 @@
 import "dotenv/config";
-import { PrismaClient } from "@prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import pkg from "@prisma/client";
 
-const databaseUrl = new URL(process.env.DATABASE_URL);
-const adapter = new PrismaMariaDb({
-  host: databaseUrl.hostname,
-  port: Number(databaseUrl.port),
-  user: decodeURIComponent(databaseUrl.username),
-  password: decodeURIComponent(databaseUrl.password),
-  database: databaseUrl.pathname.slice(1),
-  ssl: {
-    rejectUnauthorized: false,
-  },
-  connectTimeout: 5000,
-});
-const prisma = new PrismaClient({ adapter });
+const { PrismaClient } = pkg;
+
+const prisma = new PrismaClient();
 
 export default prisma;
