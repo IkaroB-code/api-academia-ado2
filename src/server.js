5a5edc23373e882d9e75cc7c95083b3cdce26d90
IkaroB-code <ikarooliveirabispo@gmail.com>
2026-06-07 04:43:27 -0300

    corrige binding da porta para Render

--- src/server.js
+++ src/server.js
@@ -34,6 +34,6 @@ app.use("/instrutores", instrutorRoutes);
 
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
