dbb05bc9ae8d69391dccc3689bd620495d729450
IkaroB-code <ikarooliveirabispo@gmail.com>
2026-06-07 23:28:55 -0300

    fix prisma client initialization options

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -3,6 +3,9 @@ import pkg from "@prisma/client";
 
 const { PrismaClient } = pkg;
 
-const prisma = new PrismaClient(); // sem datasources
+const prisma = new PrismaClient({
+    errorFormat: "minimal", // ou "colorless", "pretty"
+    log: ["error", "warn"], // níveis de log permitidos
+}); // sem datasources
 
 export default prisma;
